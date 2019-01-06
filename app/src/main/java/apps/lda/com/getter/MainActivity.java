058a2415756408dd8fbaa54ebe5c1702e6a4a8ff
ArnoXX <tbolz2112@gmail.com>
2019-01-06 14:40:19 +0200

    Added ShadowUtil

--- app/src/main/java/apps/lda/com/getter/MainActivity.java
+++ app/src/main/java/apps/lda/com/getter/MainActivity.java
@@ -2,6 +2,11 @@ package apps.lda.com.getter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import apps.lda.com.getter.utils.ShadowUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate (savedInstanceState);
         getSupportActionBar().hide();
         setContentView (R.layout.activity_main);
+        RelativeLayout searchBar = findViewById (R.id.searchbar);
+        searchBar.setBackground (ShadowUtil.getShadow (searchBar, R.color.darkgrey, R.dimen.zeroBorderRadius, R.color.shadow, R.dimen.elevation, Gravity.CENTER));
+        LinearLayout breadcrumbs = findViewById (R.id.breadcrumbs);
+        breadcrumbs.setBackground (ShadowUtil.getShadow (breadcrumbs, R.color.grey, R.dimen.zeroBorderRadius, R.color.shadow, R.dimen.elevation, Gravity.CENTER));
+
     }
 }

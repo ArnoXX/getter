6ed1ed91620dc3060b61b6291fd8bdd07ab5b053
ArnoXX <tbolz2112@gmail.com>
2019-01-07 14:06:41 +0200

    Fuck custom shadows - now using elevation

--- app/src/main/java/apps/lda/com/getter/MainActivity.java
+++ app/src/main/java/apps/lda/com/getter/MainActivity.java
@@ -6,8 +6,6 @@ import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
-import apps.lda.com.getter.utils.ShadowUtil;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -15,10 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate (savedInstanceState);
         getSupportActionBar().hide();
         setContentView (R.layout.activity_main);
-        RelativeLayout searchBar = findViewById (R.id.searchbar);
-        searchBar.setBackground (ShadowUtil.getShadow (searchBar, R.color.darkgrey, R.dimen.zeroBorderRadius, R.color.dark, R.dimen.elevation, Gravity.TOP));
-        LinearLayout breadcrumbs = findViewById (R.id.breadcrumbs);
-        breadcrumbs.setBackground (ShadowUtil.getShadow (breadcrumbs, R.color.grey, R.dimen.zeroBorderRadius, R.color.shadow, R.dimen.elevation, Gravity.TOP));
-
     }
 }

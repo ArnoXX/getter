52221241d8b1a583e1562a632503c2ebc35165ee
ArnoXX <tbolz2112@gmail.com>
2019-01-08 23:43:15 +0200

    brainfuck

--- app/src/main/java/apps/lda/com/getter/MainActivity.java
+++ app/src/main/java/apps/lda/com/getter/MainActivity.java
@@ -1,39 +1,48 @@
 package apps.lda.com.getter;
 
-import android.graphics.drawable.Drawable;
+import android.content.Context;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.Gravity;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-
-import apps.lda.com.getter.customViews.ExplorerElementView;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static int fromDpToPx(float dp){
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 320f);
+        return Math.round(px);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         getSupportActionBar().hide();
         setContentView (R.layout.activity_main);
+        // TODO complete this shit
+        TextView btn  = new TextView (this);
+        ScrollView mainScroll = findViewById (R.id.mainScroll);
+        LinearLayout mainScrollLayout = findViewById (R.id.mainScrollLines);
+        RelativeLayout element = new RelativeLayout (this);
 
-        LinearLayout mainScrollLines = findViewById (R.id.mainScrollLines);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams (ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.setMargins (0, fromDpToPx (getResources ().getDimension (R.dimen.explorerElementViewTopMargin)), 0, 0);
+        btn.setText (String.format ("%d", fromDpToPx (getResources ().getDimension (R.dimen.explorerElementViewTopMargin))));
+        params1.addRule (RelativeLayout.CENTER_IN_PARENT);
+        btn.setLayoutParams (params1);
+        mainScrollLayout.setClipToPadding (false);
+        element.setBackground(getDrawable (R.drawable.explorer_element_view_bg));
+        element.setElevation (fromDpToPx (getResources ().getDimension (R.dimen.elevation)));
 
-       LinearLayout.LayoutParams params = new LinearLayout.LayoutParams (LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-       params.setMargins (fromDpToPx (R.dimen.explorerElementViewLeftMargin),fromDpToPx ( R.dimen.explorerElementViewTopMargin), fromDpToPx (R.dimen.explorerElementViewRightMargin), fromDpToPx (R.dimen.explorerElementViewBottomMargin));
-
-       Drawable bg = getDrawable (R.drawable.explorer_element_view_bg);
-
-        ExplorerElementView element = new ExplorerElementView (this, bg);
-        element.setElevation (fromDpToPx (R.dimen.elevation));
         element.setLayoutParams (params);
 
+        element.addView (btn);
 
-
-    }
-    private int fromDpToPx(int dp){
-        return Math.round(dp*(getResources().getDisplayMetrics().xdpi/ DisplayMetrics.DENSITY_DEFAULT));
+        mainScrollLayout.addView (element);
     }
 }
